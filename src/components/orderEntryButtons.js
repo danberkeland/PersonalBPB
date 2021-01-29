6a7251c80e2aba5f42998f1c8e90083bdb10ce7b
Dan Berkeland <danberkeland@gmail.com>
2021-01-29 09:22:38 -0800

    Clear Order button works

--- src/components/orderEntryButtons.js
+++ src/components/orderEntryButtons.js
@@ -1,25 +1,35 @@
 import React, { useContext } from 'react';
 import { CustDateRecentContext } from '../dataContexts/CustDateRecentContext';
+import { OrdersContext } from '../dataContexts/OrdersContext';
 
 
 function OrderEntryButtons() {
 
   const { orderType, setOrderType } = useContext(CustDateRecentContext)
   const { setChosen } = useContext(CustDateRecentContext)
+  const { thisOrder, setThisOrder } = useContext(OrdersContext)
 
   let type = orderType ? "Special" : "Whole";
 
-  const handleClick = () => {
+  const handleChangeOrderType = () => {
     setOrderType(!orderType)
     setChosen('')
+  }
+
+  const handleClear = () => {
+    let newThisOrder = [...thisOrder]
+    newThisOrder = newThisOrder.map(order => ["0",order[1],order[2]])
+    console.log(newThisOrder)
+    setThisOrder(newThisOrder);
+
   }
 
   return (         
     <div className = "orderEntryButtons">
       <button>Add/Update</button>
-      <button>Clear Order</button>
+      <button onClick={handleClear}>Clear Order</button>
       <button>Standing</button>
-      <button onClick={handleClick}>{type} Order</button>
+      <button onClick={handleChangeOrderType}>{type} Order</button>
     </div>    
   );
 }

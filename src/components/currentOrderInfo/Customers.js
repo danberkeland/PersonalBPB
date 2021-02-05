db9bae88b59d72cdf881bc652544c8e13a2027e9
Dan Berkeland <danberkeland@gmail.com>
2021-02-05 14:59:34 -0800

    fixed development dependancy for useEffect

--- src/components/currentOrderInfo/Customers.js
+++ src/components/currentOrderInfo/Customers.js
@@ -1,5 +1,4 @@
-import React, { useContext, useState } from 'react';
-import { useEffect } from 'react/cjs/react.development';
+import React, { useContext, useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import { CurrentDataContext } from '../../dataContexts/CurrentDataContext';

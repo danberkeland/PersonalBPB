ef28a8b944ef5ced9c46cd933a9a8694e1efddb8
Dan Berkeland <danberkeland@gmail.com>
2021-02-05 05:55:45 -0800

    Command Line add Product is starting to function

--- src/components/orderCommandLine.js
+++ src/components/orderCommandLine.js
@@ -2,17 +2,19 @@ import React, { useContext } from 'react';
 import { CurrentDataContext } from '../dataContexts/CurrentDataContext';
 import { OrdersContext } from '../dataContexts/OrdersContext';
 import { CustomerContext } from '../dataContexts/CustomerContext';
+import { ProductsContext } from '../dataContexts/ProductsContext';
 
-import { todayPlus, daysOfTheWeek } from '../helpers/dateTimeHelpers'
+import { todayPlus, daysOfTheWeek, convertDatetoBPBDate } from '../helpers/dateTimeHelpers'
 
 import swal from '@sweetalert/with-react';
 
 
 const OrderCommandLine = () => {
 
-  const { chosen, setChosen, delivDate, setDelivDate, setorderTypeWhole } = useContext(CurrentDataContext)
+  const { chosen, setChosen, delivDate, setDelivDate, orderTypeWhole, setorderTypeWhole, route, ponote } = useContext(CurrentDataContext)
   const { orders, setOrders } = useContext(OrdersContext)
   const { customers } = useContext(CustomerContext)
+  const { products } = useContext(ProductsContext)
   
   const checkForCustomer = (entry, customers) => {
 
@@ -71,59 +73,64 @@ const OrderCommandLine = () => {
     }
   };
 
-
-  const checkForRoute = (entry) => {
-    // construct a list based on entry find for chosen and deliv date
-    // if no list
-    //    retrieve default route from customer profile
-    // else
-    //    retrieve route from list
-    return ''
-  };
-
-
-  const checkForPonotes = (entry) => {
-    
-
-    // construct a list based on entry find for chosen and deliv date
-    // if no list
-    //    ponote = ''
-    // else
-    //    retrieve ponote from list
-    return ''
-  };
- 
   
   
-  const checkForProducts = (entry, newRoute, newPonote) => {
+  const checkForProducts = (entry) => {
     
-    /*}
+    
     let isThereAProduct = /\d+\s\w+/g.test(entry)
-   
     if (isThereAProduct){
+      let newOrder
       const array = [...entry.matchAll(/\d+\s\w+/g)];
       let enteredProducts = array.map(item => item[0].split(" "))
       let ordersToUpdate = [];
       for (let prod of products){
         for (let item of enteredProducts){
           if (prod[2] === item[1]){
-            let newOrder = [item[0],prod[1], chosen, newPonote, newRoute, item[0], orderTypeWhole] // [ qty, prod, cust, po, route, so, ty ]
+            newOrder = [item[0],prod[1], chosen, ponote, route, item[0], orderTypeWhole, convertDatetoBPBDate(delivDate)] // [ qty, prod, cust, po, route, so, ty ]
             ordersToUpdate.push(newOrder)
           }
   
         }
       }
-    */
-    return []
+    
+      console.log(ordersToUpdate)
+
+      // create map of orders for cust, delivdate
+
+      let custOrderList = orders.filter(order => order[7] === convertDatetoBPBDate(delivDate) && order[2] === chosen)
+      console.log(custOrderList)
+
+      // new product by new product, check if it exists
+      let ord
+      if (custOrderList.length<=0){
+        console.log("order Does not exist")
+        let ordersToModify = [...orders]
+        for (ord of ordersToUpdate){
+          ordersToModify.push(ord)
+        }
+        setOrders(ordersToModify)
+        return
+      }
+
+
+      for (let ord of ordersToUpdate){
+        for (let custOrd of custOrderList){
+          if (ord[1] === custOrd[1]){
+            console.log("order Exists")
+            //      if exists, modify qty
+            return
+          } 
+        }
+      }
+    }
   }
   
-  
   const interpretEntry = async (entry) => {
-    let isCust = checkForCustomer(entry, customers)
+    checkForCustomer(entry, customers)
+
     checkForDelivDate(entry)
-    if (!isCust){
-      return
-    }   
+    checkForProducts(entry)
   };
 
   

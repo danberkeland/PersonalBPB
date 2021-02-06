3f121477ff3308f604ab94fbc0886bebc5692246
Dan Berkeland <danberkeland@gmail.com>
2021-02-06 14:44:33 -0800

    standing orders are grabbed and presented

--- src/components/currentOrderList/StandingOrder/StandingOrderEntry.js
+++ src/components/currentOrderList/StandingOrder/StandingOrderEntry.js
@@ -2,6 +2,8 @@ import React, { useEffect, useContext, useState } from 'react';
 
 import swal from '@sweetalert/with-react';
 
+import { v4 as uuidv4 } from 'uuid';
+
 import { OrdersContext } from '../../../dataContexts/OrdersContext';
 import { StandingContext } from '../../../dataContexts/StandingContext';
 import { CurrentDataContext } from '../../../dataContexts/CurrentDataContext';
@@ -12,9 +14,31 @@ const clonedeep = require('lodash.clonedeep')
 
 const StandingOrderEntry = () => {
 
-    const standArray = [["Baguette","2","3","3","4","5","6","2"],
-                        ["Croissant","2","3","3","4","5","6","2"],
-                        ["Bunz","2","3","3","4","5","6","2"]]
+    const [ standArray, setStandArray ] = useState()
+
+    const { standing } = useContext(StandingContext);
+    const { chosen } = useContext(CurrentDataContext);
+
+    useEffect(() => {
+        let buildStandArray = []
+        // item for item in standing
+        let pullStand = clonedeep(standing)
+        for (let pull of pullStand){
+            // search index of item in buildArray
+            if (pull[8] === chosen){
+                let ind = buildStandArray.findIndex(stand => stand[0] === pull[7])
+                if (ind>=0){
+                    buildStandArray[ind][Number(pull[0])] = pull[2]
+                } else {
+                    let newStand = [pull[7],"0","0","0","0","0","0","0"]
+                    newStand[Number(pull[0])] = pull[2]
+                    buildStandArray.push(newStand)
+                    
+                }
+            }
+        }    
+        setStandArray(buildStandArray)
+    },[chosen, standing])
 
     return (
         <React.Fragment> 
@@ -29,16 +53,16 @@ const StandingOrderEntry = () => {
             <label></label>
 
             {standArray ? standArray.map(order =>
-                (<React.Fragment>
-                    <label>{order[0]}</label>
+                (<React.Fragment key={order[0]+"frag"}>
+                    <label key={order[0]+"prod"}>{order[0]}</label>
 
-                    <input type="text" size="3" maxLength="3" placeholder={order[1]} ></input>
-                    <input type="text" size="3" maxLength="3" placeholder={order[2]} ></input>
-                    <input type="text" size="3" maxLength="3" placeholder={order[3]} ></input>
-                    <input type="text" size="3" maxLength="3" placeholder={order[4]} ></input>
-                    <input type="text" size="3" maxLength="3" placeholder={order[5]} ></input>
-                    <input type="text" size="3" maxLength="3" placeholder={order[6]} ></input>
-                    <input type="text" size="3" maxLength="3" placeholder={order[7]} ></input>
+                    <input type="text" key={order[0]+"sun"} size="3" maxLength="3" placeholder={order[1]} ></input>
+                    <input type="text" key={order[0]+"mon"} size="3" maxLength="3" placeholder={order[2]} ></input>
+                    <input type="text" key={order[0]+"tues"} size="3" maxLength="3" placeholder={order[3]} ></input>
+                    <input type="text" key={order[0]+"wed"} size="3" maxLength="3" placeholder={order[4]} ></input>
+                    <input type="text" key={order[0]+"thurs"} size="3" maxLength="3" placeholder={order[5]} ></input>
+                    <input type="text" key={order[0]+"fri"} size="3" maxLength="3" placeholder={order[6]} ></input>
+                    <input type="text" key={order[0]+"sat"} size="3" maxLength="3" placeholder={order[7]} ></input>
     
                     <button>X</button>
                 </React.Fragment>)) : ''}

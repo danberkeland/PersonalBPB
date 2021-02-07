9dc9a053409ebb9f053b4734b3bc5e77e25f2d36
Dan Berkeland <danberkeland@gmail.com>
2021-02-07 14:09:15 -0800

    Standing order works.  Need to test more fully.

--- src/components/currentOrderList/StandingOrder/AddStandingOrderEntry.js
+++ src/components/currentOrderList/StandingOrder/AddStandingOrderEntry.js
@@ -4,17 +4,19 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { CurrentDataContext } from '../../../dataContexts/CurrentDataContext';
 import { OrdersContext } from '../../../dataContexts/OrdersContext';
+import { StandingContext } from '../../../dataContexts/StandingContext';
 import { ProductsContext } from '../../../dataContexts/ProductsContext'
 
 import { convertDatetoBPBDate } from '../../../helpers/dateTimeHelpers';
 import { findAvailableProducts,decideWhetherToAddOrModify } from '../../../helpers/sortDataHelpers';
 
-
+const clonedeep = require('lodash.clonedeep')
 
 
 const AddCartEntryItem = () => {
 
     const { products } = useContext(ProductsContext)
+    const { standing, setStanding } = useContext(StandingContext)
     const { orders, setOrders } = useContext(OrdersContext)
     const { chosen, delivDate, orderTypeWhole, route, ponote } = useContext(CurrentDataContext)
 
@@ -34,11 +36,13 @@ const AddCartEntryItem = () => {
     } 
 
     const handleAdd = () => {
-        let qty = document.getElementById("addedProdQty").value
-        let newOrder =[qty, pickedProduct, chosen, ponote, route, qty, orderTypeWhole, convertDatetoBPBDate(delivDate)] 
-        let newOrderList = decideWhetherToAddOrModify(orders, newOrder, delivDate)
-        setOrders(newOrderList)
-        document.getElementById("addedProdQty").value = '';
+        let newStandingList = clonedeep(standing)
+        for (let i=1; i<8; i++){
+            let newOrder =[i.toString(),"na","0","na","na","na","na",pickedProduct,chosen] 
+            newStandingList.push(newOrder)
+        }
+        
+        setStanding(newStandingList)
         setPickedProduct('');
     }
 

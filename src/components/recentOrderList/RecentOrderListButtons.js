606319021eab35cb8c794c7257a8b3c40b3202cc
Dan Berkeland <danberkeland@gmail.com>
2021-02-07 10:32:03 -0800

    Mock upload of orders from RecentOrderListBUtton is working

--- src/components/recentOrderList/RecentOrderListButtons.js
+++ src/components/recentOrderList/RecentOrderListButtons.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React,{ useContext } from 'react';
+
+import { OrdersContext } from '../../dataContexts/OrdersContext';
+
+
+require('dotenv').config()
 
 
 const RecentOrderListButtons = () => {
 
+  const { orders } = useContext(OrdersContext)
+
+  const handleUpload = () => {
+    //turn orders into json object - data
+    let orderData = [...orders]
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type':'application/json'},
+      body: JSON.stringify(orderData)
+    };
+
+    fetch(process.env.REACT_APP_API_SENDORDERS, requestOptions)
+      .then(response => response.json())
+      .then(data => console.log(data))
+
+  }
 
   return (
       <React.Fragment>      
         <div>
-        <button className = "recentOrderListButton">Upload</button>
+        <button className = "recentOrderListButton" onClick={handleUpload}>Upload</button>
         </div>
     </React.Fragment>  
   );

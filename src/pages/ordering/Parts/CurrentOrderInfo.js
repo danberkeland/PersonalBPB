1a2b8a16da43c9e28efcd3e98c649f4ce0497b5a
Dan Berkeland <danberkeland@gmail.com>
2021-02-25 05:10:34 -0800

    deliv or pickup decider

--- src/pages/ordering/Parts/CurrentOrderInfo.js
+++ src/pages/ordering/Parts/CurrentOrderInfo.js
@@ -8,6 +8,9 @@ import { RadioButton } from 'primereact/radiobutton';
 
 import styled from 'styled-components'
 import { CustomerContext } from '../../../dataContexts/CustomerContext';
+import { CurrentDataContext } from '../../../dataContexts/CurrentDataContext';
+import { OrdersContext } from '../../../dataContexts/OrdersContext';
+import { StandingContext } from '../../../dataContexts/StandingContext';
 
 const CurrentOrderInfo = () => {
 
@@ -44,19 +47,20 @@ const CurrentOrderInfo = () => {
     `
   const FulfillOptions = styled.div`
     display: grid;
-    grid-template-columns: 3fr 2fr 1fr 2fr 1fr 2fr 1fr;
+    grid-template-columns: 3fr 3fr 1fr 3fr 1fr 3fr 1fr;
     margin: 10px;
     align-items: center;
     justify-items: center;
     `
-
-  const delivDate = "01/02/2021"
-
-
+    
   const {cartList, standList, orderTypeWhole } = useContext(ToggleContext)
   const { customers } = useContext(CustomerContext)
+  const { chosen, setChosen, delivDate } = useContext(CurrentDataContext)
+  const { orders } = useContext(OrdersContext)
+  const { standing } = useContext(StandingContext)
 
   const [ customerGroup, setCustomerGroup ] = useState(customers)
+  const [ delivStatus, setDelivStatus ] = useState('slopick')
 
 
   const ho = {
@@ -73,11 +77,40 @@ const CurrentOrderInfo = () => {
   
 
     useEffect(() => {
-        setCustomerGroup(customers)
+      setCustomerGroup(customers)
     },[ customers ])
 
 
+    // Figure out if current order is delivery or pickup
+    useEffect(() => {
+      let custRoute
+      let currentOrderList = buildCurrentOrder(chosen,delivDate,orders,standing)
+      if (currentOrderList.length>0){
+        custRoute = currentOrderList[0]["route"]
+      } else {
+        custRoute = customers.filter(cust => cust["name"]===chosen)[0]["route"]
+      }
 
+      switch(custRoute){
+        case "Pick up SLO":
+          setDelivStatus('slopick');
+          break;
+        case "Pick up Carlton":
+          setDelivStatus('atownpick');
+          break;
+        default:
+          setDelivStatus('deliv');
+          break;
+      }    
+    }, [chosen, delivDate, customers, orders, standing])
+   
+
+
+    const changeDate = (date) => {
+      let currentDate = new Date(date);
+      var fd = currentDate.toDateString();
+      return fd;
+    }
 
   return (   
     <React.Fragment>
@@ -85,7 +118,7 @@ const CurrentOrderInfo = () => {
       <React.Fragment> 
         <TitleBox>
           <Title style={cartList ? so : standList ? so : ho }>Wholesale {orderType} Order</Title>
-          <DateStyle>Tues, March 12</DateStyle>
+          <DateStyle>{delivDate ? changeDate(delivDate) : ''}</DateStyle>
         </TitleBox>
       </React.Fragment> : <h2 style={standList ? so : ho }>Retail {orderType} Order</h2>}
 
@@ -93,13 +126,19 @@ const CurrentOrderInfo = () => {
         <CurrentOrderInfo>
          
           <FulfillOptions> 
-            <Dropdown id="customers" options={customerGroup} optionLabel="name" placeholder="Select a customer"/>     
+            <Dropdown id="customers" 
+              value={chosen} options={customerGroup} 
+              optionLabel="name" placeholder="Select a customer"
+              onChange={(e) => setChosen(e.value)}/>     
             <label htmlFor="delivery">Delivery</label>
-            <RadioButton value="deliv" name="delivery" />
+            <RadioButton value="deliv" name="delivery" 
+              onChange={(e) => setDelivStatus(e.value)} checked={delivStatus === 'deliv'}/>
             <label htmlFor="pickupSLO">Pick up SLO</label>
-            <RadioButton value="slopick" name="delivery" />
+            <RadioButton value="slopick" name="delivery"
+              onChange={(e) => setDelivStatus(e.value)} checked={delivStatus === 'slopick'} />
             <label htmlFor="pickupAtown">Pick up Carlton</label>
-            <RadioButton value="atownpick" name="delivery" />
+            <RadioButton value="atownpick" name="delivery" 
+              onChange={(e) => setDelivStatus(e.value)} checked={delivStatus === 'atownpick'}/>
           </FulfillOptions>
           <SpecialInfo>
             <span className="p-float-label">

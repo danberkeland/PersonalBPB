0e04a3fa16d84e079afd61bbd5b1ddddf6b426b3
Dan Berkeland <danberkeland@gmail.com>
2021-03-06 07:04:42 -0800

    working on customer page

--- src/pages/customers/Customers.js
+++ src/pages/customers/Customers.js
@@ -1,9 +1,10 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import styled from 'styled-components'
 
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import { ScrollPanel } from 'primereact/scrollpanel';
 
 import { CustomerContext, CustomerLoad } from '../../dataContexts/CustomerContext'
 import { ProductsContext } from '../../dataContexts/ProductsContext'
@@ -17,11 +18,20 @@ import { HoldingContext } from '../../dataContexts/HoldingContext';
 
 function Customers() {
 
+  const [ selectedCustomer, setSelectedCustomer ] = useState(null)
+
   const { customers, custLoaded, setCustLoaded } = useContext(CustomerContext)
   const { setProdLoaded } = useContext(ProductsContext)
   let { setHoldLoaded } = useContext(HoldingContext)
   let { setOrdersLoaded } = useContext(OrdersContext)
   let { setStandLoaded } = useContext(StandingContext)
+
+  
+
+  const handleSelection = e => {
+    setSelectedCustomer(e.value)
+    console.log(e.value)
+  }
 
 
   useEffect(() => {
@@ -35,26 +45,40 @@ function Customers() {
     setStandLoaded(true)
   },[])
 
-  
-
-
   const MainWrapper = styled.div`
-  font-family: 'Montserrat', sans-serif;
-  margin: auto;
-  width: 100%;
-  height: 100%;
-  background: #ffffff;
-  `
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    `
 
+
+  const ListWrapper = styled.div`
+    font-family: 'Montserrat', sans-serif;
+    margin: auto;
+    width: 100%;
+    height: 100%;
+    background: #ffffff;
+    `
+
+  
   return (
     <React.Fragment>
        {!custLoaded ? <CustomerLoad /> : ''}
-        <MainWrapper>
-          <DataTable value={customers} className="p-datatable-striped" selectionMode="single" dataKey="id">
-            <Column field="custName" header="Customer"sortable filter filterPlaceholder="Search by name"></Column>
-            <Column field="nickName" header="Nickname"sortable filter filterPlaceholder="Search by nickname"></Column>
+      <MainWrapper>
+        <ListWrapper>
+         
+          
+         <ScrollPanel style={{ width: '100%', height: '100vh' }}>
+          <DataTable value={customers} className="p-datatable-striped" 
+            selection={selectedCustomer} onSelectionChange={handleSelection} selectionMode="single" dataKey="id">
+            <Column field="custName" header="Customer" sortable filter filterPlaceholder="Search by name"></Column>
+            <Column field="nickName" header="Nickname" sortable filter filterPlaceholder="Search by nickname"></Column>
           </DataTable>
-      </MainWrapper> 
+          </ScrollPanel>
+          
+          
+      </ListWrapper> 
+      <h1>{selectedCustomer && selectedCustomer.custName}</h1>
+    </MainWrapper>
     </React.Fragment>         
   );
 }

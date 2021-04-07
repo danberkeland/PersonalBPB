707d0ea906d74d3b8307a35b1c54072277bf579f
Dan Berkeland <danberkeland@gmail.com>
2021-04-07 15:09:48 -0700

    basic pdf printer

--- src/pages/logistics/ByRoute/Parts/RouteGrid.js
+++ src/pages/logistics/ByRoute/Parts/RouteGrid.js
@@ -1,6 +1,10 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, useRef } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
+import { Button } from 'primereact/button';
+
+import jsPDF from 'jspdf'
+import 'jspdf-autotable'
 
 import { ProductsContext } from "../../../../dataContexts/ProductsContext";
 import { OrdersContext } from "../../../../dataContexts/OrdersContext";
@@ -17,6 +21,8 @@ const RouteGrid = ({ route, orderList }) => {
   const { products } = useContext(ProductsContext);
   const { orders } = useContext(OrdersContext);
   const { standing } = useContext(StandingContext);
+
+  const dt = useRef(null);
 
   const [columns, setColumns] = useState([]);
   const [data, setData] = useState([]);
@@ -67,10 +73,26 @@ const RouteGrid = ({ route, orderList }) => {
     );
   });
 
+  const exportColumns = columns.map(col => ({ title: col.header, dataKey: col.field }));
+
+  const exportPdf = () => {
+    const doc = new jsPDF('landscape');
+    doc.autoTable(exportColumns, data);
+    doc.save("products.pdf");
+  };
+
+  const header = (
+    <div className="p-d-flex export-buttons">
+        
+        <Button type="button" icon="pi pi-file-pdf" onClick={exportPdf} className="p-button-warning p-mr-2" data-pr-tooltip="PDF" />
+       
+    </div>
+);
+
   return (
     <div>
       <div className="card">
-        <DataTable
+        <DataTable header={header} ref={dt}
           className="p-datatable-gridlines p-datatable-sm p-datatable-striped"
           value={data}
           resizableColumns

90d183758e3d0847102e60aa7169a64129bef94f
Dan Berkeland <danberkeland@gmail.com>
2021-04-14 10:43:11 -0700

    gettin there with dough calc.  Trickier than it seems

--- src/pages/settings/editDough/Info.js
+++ src/pages/settings/editDough/Info.js
@@ -34,16 +34,17 @@ const Info = ({
   .filter((dgh) => dgh.dough === selectedDough.doughName && dgh.componentType === "dryplus")
   .map((dg) => ({ ing: dg.componentName }));
 
+  
+
   const deleteButton = () => {
     return <Button icon="pi pi-times"
     className="p-button-outlined p-button-rounded p-button-help p-button-sm" />;
   };
 
   const handleInput = (e) => {
-    console.log(e)
+    
     let placeholder = doughComponents
       .filter((dgh) => dgh.dough === selectedDough.doughName && dgh.componentName === e.ing)[0].amount
-    console.log(placeholder)
     return (
       <InputText
         id="firstname3"
@@ -55,6 +56,60 @@ const Info = ({
     );
   };
 
+  
+
+  const getFlourWeight = (e) => {
+    let bulkWeight = selectedDough.batchSize;
+    let hydro = Number(selectedDough.hydration);
+    let levNum = 0;
+    doughComponents
+      .filter(
+        (dgh) =>
+          dgh.dough === selectedDough.doughName && dgh.componentType === "pre"
+      )
+      .forEach((element) => {
+        levNum = levNum + element.amount;
+      });
+    let addNum = 0;
+    doughComponents
+      .filter(
+        (dgh) =>
+          dgh.dough === selectedDough.doughName &&
+          dgh.componentType === "dryplus"
+      )
+      .forEach((element) => {
+        addNum = addNum + element.amount;
+      });
+    
+    
+    let fl = (
+      bulkWeight /
+      (1 + hydro * 0.01 + levNum * 0.01 + addNum * 0.01)
+    ).toFixed(2);
+
+    return fl;
+  };
+
+
+  const dryWeight =(e) => {
+    let fl = getFlourWeight(e)
+    let thisAmount = doughComponents.filter(
+      (dgh) =>
+        dgh.dough === selectedDough.doughName && dgh.componentName === e.ing
+    )[0].amount;
+    return (fl*.01*thisAmount).toFixed(2)
+  }
+
+  const wetWeight =(e) => {
+    let fl = getFlourWeight(e)
+    let hydro = Number(selectedDough.hydration);
+    let thisAmount = doughComponents.filter(
+      (dgh) =>
+        dgh.dough === selectedDough.doughName && dgh.componentName === e.ing
+    )[0].amount;
+    return (fl*.01*thisAmount*hydro*.01).toFixed(2)
+  }
+  
   return (
     <React.Fragment>
       <div className="p-grid p-ai-center">
@@ -90,10 +145,9 @@ const Info = ({
             <Column
               className="p-text-center"
               header="% of flour weight"
-              id="dryplus"
               body={handleInput}
             ></Column>
-            <Column className="p-text-center" header="Weight"></Column>
+            <Column className="p-text-center" header="Weight" body={dryWeight}></Column>
             <Column className="p-text-center" header="Total %"></Column>
             <Column className="p-text-right" body={deleteButton}></Column>
           </DataTable>
@@ -108,7 +162,7 @@ const Info = ({
               header="Parts Dry"
               body={handleInput}
             ></Column>
-            <Column className="p-text-center" header="Weight"></Column>
+            <Column className="p-text-center" header="Weight" body={dryWeight}></Column>
             <Column className="p-text-center" header="100%"></Column>
             <Column className="p-text-right" body={deleteButton}></Column>
           </DataTable>
@@ -123,7 +177,7 @@ const Info = ({
               header="Parts Wet"
               body={handleInput}
             ></Column>
-            <Column className="p-text-center" header="Weight"></Column>
+            <Column className="p-text-center" header="Weight" body={wetWeight}></Column>
             <Column
               className="p-text-center"
               header={Number(selectedDough.hydration) + "%"}
@@ -141,7 +195,7 @@ const Info = ({
               header="% flour weights"
               body={handleInput}
             ></Column>
-            <Column className="p-text-center" header="Weight"></Column>
+            <Column className="p-text-center" header="Weight" body={dryWeight}></Column>
             <Column className="p-text-center" header="Total %"></Column>
             <Column className="p-text-right" body={deleteButton}></Column>
             

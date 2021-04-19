a400a35d8c30277e36cd55c726440baada4b64fc
Dan Berkeland <danberkeland@gmail.com>
2021-04-19 10:00:26 -0700

    progress on eod count page

--- src/pages/EODCounts/BPBSCounts.js
+++ src/pages/EODCounts/BPBSCounts.js
@@ -1,10 +1,33 @@
 import React, { useContext, useEffect, useState } from "react";
 
 import { CustomerContext } from "../../dataContexts/CustomerContext";
-import { ProductsContext, ProductsLoad } from "../../dataContexts/ProductsContext";
+import {
+  ProductsContext,
+  ProductsLoad,
+} from "../../dataContexts/ProductsContext";
 import { OrdersContext } from "../../dataContexts/OrdersContext";
 import { StandingContext } from "../../dataContexts/StandingContext";
 import { HoldingContext } from "../../dataContexts/HoldingContext";
+
+import styled from "styled-components";
+
+const WholeBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 40%;
+  margin: auto;
+`;
+
+const BagGrid = styled.div`
+  display: grid;
+  grid-template-columns: 3fr 1fr 1fr;
+`;
+
+const EaGrid = styled.div`
+  display: grid;
+
+  grid-template-columns: 3fr 1fr;
+`;
 
 function BPBSCounts() {
   const { products, prodLoaded, setProdLoaded } = useContext(ProductsContext);
@@ -13,7 +36,7 @@ function BPBSCounts() {
   let { setOrdersLoaded } = useContext(OrdersContext);
   let { setStandLoaded } = useContext(StandingContext);
 
-  const [ eodProds, setEODProds ] = useState()
+  const [eodProds, setEODProds] = useState();
 
   useEffect(() => {
     if (!products) {
@@ -26,20 +49,97 @@ function BPBSCounts() {
   }, []);
 
   useEffect(() => {
-    let prodsToMap = products.filter(prod => prod.bakedWhere[0] === "Prado" && prod.eodCount===true)
-    setEODProds(prodsToMap)
-  },[products])
-  
+    let prodsToMap = products.filter(
+      (prod) => prod.bakedWhere[0] === "Prado" && prod.eodCount === true
+    );
+    setEODProds(prodsToMap);
+  }, [products]);
 
   return (
     <React.Fragment>
-      {!prodLoaded ? <ProductsLoad /> : ""}
-      <h1>BPBS EOD Counts</h1>
-      {eodProds ? eodProds.map(eod => (
-        <div key={eod.prodName}>{eod.prodName}</div>
-      )) : ''}
-      
-      
+      <WholeBox>
+        {!prodLoaded ? <ProductsLoad /> : ""}
+        <h1>BPBS EOD Counts</h1>
+        <h2>On Shelf</h2>
+
+        <BagGrid>
+          <h3>By Bag</h3>
+          <h3># of bags</h3>
+          <h3>ea</h3>
+          {eodProds
+            ? eodProds
+                .filter(
+                  (prods) =>
+                    prods.freezerThaw !== true && Number(prods.packSize) > 1
+                )
+                .map((eod) => (
+                  <React.Fragment>
+                    <div key={eod.prodName}>{eod.prodName}</div>
+                    <div>123</div>
+                    <div>123</div>
+                  </React.Fragment>
+                ))
+            : ""}
+        </BagGrid>
+
+        <EaGrid>
+          <h3>Each</h3>
+          <h3>ea</h3>
+          {eodProds
+            ? eodProds
+                .filter(
+                  (prods) =>
+                    prods.freezerThaw !== true && Number(prods.packSize) === 1
+                )
+                .map((eod) => (
+                  <React.Fragment>
+                    <div key={eod.prodName}>{eod.prodName}</div>
+                    <div>123</div>
+                  </React.Fragment>
+                ))
+            : ""}
+        </EaGrid>
+
+        <h2>In Freezer</h2>
+
+        <BagGrid>
+          <h3>By Bag</h3>
+          <h3># of bags</h3>
+          <h3>ea</h3>
+          {eodProds
+            ? eodProds
+                .filter(
+                  (prods) =>
+                    prods.freezerThaw === true && Number(prods.packSize) > 1
+                )
+                .map((eod) => (
+                  <React.Fragment>
+                    <div key={eod.prodName}>{eod.prodName}</div>
+                    <div>123</div>
+                    <div>123</div>
+                  </React.Fragment>
+                ))
+            : ""}
+        </BagGrid>
+
+        <EaGrid>
+          <h3>Each</h3>
+          <h3>ea</h3>
+          {eodProds
+            ? eodProds
+                .filter(
+                  (prods) =>
+                    prods.freezerThaw === true && Number(prods.packSize) === 1
+                )
+                .map((eod) => (
+                  <React.Fragment>
+                    <div key={eod.prodName}>{eod.prodName}</div>
+                    <div>123</div>
+                  </React.Fragment>
+                ))
+            : ""}
+        </EaGrid>
+      </WholeBox>
     </React.Fragment>
   );
 }

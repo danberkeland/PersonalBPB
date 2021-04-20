7120dfac26bba13393c5ba16f5a366abccfc5486
Dan Berkeland <danberkeland@gmail.com>
2021-04-19 19:27:58 -0700

    just need submit button on eodcount

--- src/pages/EODCounts/EODCounts.js
+++ src/pages/EODCounts/EODCounts.js
@@ -22,10 +22,10 @@ const WholeBox = styled.div`
   margin: auto;
 `;
 
-
+const clonedeep = require("lodash.clonedeep");
 
 function EODCounts({ loc }) {
-  const { products, prodLoaded, setProdLoaded } = useContext(ProductsContext);
+  const { products, setProducts, prodLoaded, setProdLoaded } = useContext(ProductsContext);
   const { setCustLoaded } = useContext(CustomerContext);
   let { setHoldLoaded } = useContext(HoldingContext);
   let { setOrdersLoaded } = useContext(OrdersContext);
@@ -50,18 +50,54 @@ function EODCounts({ loc }) {
     setEODProds(prodsToMap);
   }, [products]);
 
+
+  const updateItem = (value, itemToUpdate) => {
+    
+    itemToUpdate[
+      itemToUpdate.findIndex(
+        (item) =>
+          item.id === value.target.id 
+      )
+    ].currentStock = value.target.value;
+  };
+
+  const handleChange = (value) => {
+    if (value.code === "Enter") {
+      let itemToUpdate = clonedeep(products);  
+      updateItem(value, itemToUpdate);
+      document.getElementById(value.target.id).value = "";
+      return itemToUpdate;
+    }
+  };
+  
+  const handleBlur = (value) => {
+    let itemToUpdate = clonedeep(products);
+    if (value.target.value !== "") {
+      updateItem(value, itemToUpdate);
+    }
+    document.getElementById(value.target.id).value = "";
+    return itemToUpdate;
+  };
   
 
   const handleInput = (e) => {
     
     return (
       <InputText
+      id={e.id}
         style={{
           width: "50px",
           backgroundColor: "#E3F2FD",
           fontWeight: "bold",
         }}
         placeholder={e.currentStock}
+        onKeyUp={(e) =>
+          e.code === "Enter" &&
+          setProducts(handleChange(e))
+        }
+        onBlur={(e) =>
+          setProducts(handleBlur(e))
+        }
       />
     );
   };

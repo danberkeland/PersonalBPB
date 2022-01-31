2f8ecf66eb2b32b60059cb632a5cf5b8262566a0
Dan Berkeland <danberkeland@gmail.com>
2022-01-31 12:42:47 -0800

    doobie stuff

--- src/pages/BPBNProd/BPBNBaker1_2.js
+++ src/pages/BPBNProd/BPBNBaker1_2.js
@@ -58,7 +58,7 @@ const compose = new ComposeWhatToBake();
 
 
 const doobieStuff = [
-  { Prod: "Doobie Buns", Bucket: "YES", Mix: "NO", Bake: "NO" },
+  { Prod: "Doobie Buns", Bucket: "YES", Mix: "YES", Bake: "NO" },
   { Prod: "Siciliano", Bucket: "YES", Mix: "NO", Bake: "NO" },
 ];
 

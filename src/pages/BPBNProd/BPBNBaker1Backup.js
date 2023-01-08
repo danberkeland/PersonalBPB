44af83a929d5f8f0c6fc3df719a860c826c87d64
Dan Berkeland <danberkeland@gmail.com>
2023-01-08 10:50:35 -0800

    doobie stuff

--- src/pages/BPBNProd/BPBNBaker1Backup.js
+++ src/pages/BPBNProd/BPBNBaker1Backup.js
@@ -65,8 +65,8 @@ const doobieStuffNOT = [
 
 
 const doobieStuff = [
-  { Prod: "Doobie Buns", Bucket: "YES", Mix: !yes ? "NO" : "NO", Bake: !yes ? "NO" : "NO" },
-  { Prod: "Siciliano", Bucket: "YES", Mix: !yes ? "NO" : "NO", Bake: !yes ? "YES" : "YES" },
+  { Prod: "Doobie Buns", Bucket: "YES", Mix: !yes ? "NO" : "NO", Bake: !yes ? "YES" : "YES" },
+  { Prod: "Siciliano", Bucket: "YES", Mix: !yes ? "NO" : "NO", Bake: !yes ? "NO" : "NO" },
 ];
 
 

2539fbb3189819c999d5ac5c93ba7ce8280ba5d1
Dan Berkeland <danberkeland@gmail.com>
2023-11-12 16:28:05 -0800

    doobie stuff

--- src/pages/BPBNProd/BPBNBaker1_2.js
+++ src/pages/BPBNProd/BPBNBaker1_2.js
@@ -60,15 +60,15 @@ let yes = DateTime.now().setZone("America/Los_Angeles").ordinal%2 === 0 ? true :
 
 //for push7
 
-const doobieStuff = [
+const doobieStuffx = [
   { Prod: "Doobie Buns", Bucket: "YES", Mix: yes ? "NO" : "NO", Bake: yes ? "NO" : "NO" },
   { Prod: "Siciliano", Bucket: "YES", Mix: yes ? "NO" : "NO", Bake: yes ? "YES" : "YES" },
 ];
 
 
-const doobieStuffx = [
-  { Prod: "Doobie Buns", Bucket: "YES", Mix: yes ? "NO" : "NO", Bake: yes ? "YES" : "YES" },
-  { Prod: "Siciliano", Bucket: "YES", Mix: yes ? "YES" : "YES", Bake: yes ? "NO" : "NO" },
+const doobieStuff = [
+  { Prod: "Doobie Buns", Bucket: "YES", Mix: yes ? "NO" : "NO", Bake: yes ? "NO" : "NO" },
+  { Prod: "Siciliano", Bucket: "YES", Mix: yes ? "NO" : "NO", Bake: yes ? "NO" : "NO" },
 ];
 
 
